fa207cd6e8f293c39c19d1d977bf065b8a1aaefd
lnvithanage <lnvithanage@virtusa.com>
2022-06-13 14:56:37 +0530

    Initial commit

---
+++ src/test/java/com/core/banking/api/gateway/internetbankingapigateway/InternetBankingApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.banking.api.gateway.internetbankingapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
